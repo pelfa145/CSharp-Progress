c9c335ae40eed5df39ff2d31ef1a163df5d0971f
pelfa145 <cymig213@gmail.com>
2026-09-03 23:28:27 +0800

    did and done add product

--- Projects/Inventory Management System/Inventory.cs
+++ Projects/Inventory Management System/Inventory.cs
@@ -1,10 +1,40 @@
 public class Inventory
 {
     private List<Product> Products = new();
+    private Dictionary<int, Product> prodDict = new();
 
     public void addProduct()
     {
-        Products.Add(new Product("Mechanical Keyboard", "Peripherals", 128, 30.99, 001));
+        Console.Write("Enter product name: ");
+        string? productName = Console.ReadLine();
+        Console.Write("Enter product category: ");
+        string? category = Console.ReadLine();
+        Console.Write("Enter how much stock this product has: ");
+        int stocks = int.Parse(Console.ReadLine());
+        Console.Write("How much is this: ");
+        double price = double.Parse(Console.ReadLine());
+        int id = generateID();
+        Products.Add(new Product(productName,
+                                 category,
+                                 stocks,
+                                 price,
+                                 id));
+        prodDict.Add(id, Products[^1]);
+    }
+    private int generateID()
+    {
+        if(Products.Count == 0)
+        {
+            return 0001;
+        }
+        else
+        {
+            return Products[^1].id + 1;
+        }
+    }
+
+    public void searchProduct()
+    {
         
     }
 }
